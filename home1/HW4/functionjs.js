eb857e97a25599efaba3f78c35801f64d32d785e
unknown <nure@pornhub>
2021-01-09 14:33:57 +0200

    work1

---
+++ home1/HW4/functionjs.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 3 === 0) {
+        console.log("FizBuz")
+    } else if (i % 2 === 0) {
+        console.log("Fiz")
+    } else {
+        console.log("Buz")
+    }
+}
